2772fe076fc9b33dca9937a635a8024f292c1bc0
H3x4des <maximiliennuel@hotmail.fr>
2022-01-18 02:35:58 -0800

    :lipstick: Integration V1

---
+++ src/scripts/js/result/result.service.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ResultService = void 0;
+var ResultService = /** @class */ (function () {
+    function ResultService() {
+    }
+    ResultService.prototype.addResult = function (newResult) {
+    };
+    ResultService.prototype.seenResult = function (idResult) {
+    };
+    ResultService.prototype.unseenResult = function (idResult) {
+    };
+    ResultService.prototype.getAllResult = function () {
+        return [];
+    };
+    ResultService.prototype.getAllResultSeen = function () {
+        return [];
+    };
+    ResultService.prototype.getAllResultUnSeen = function () {
+        return [];
+    };
+    ResultService.prototype.numberOfEventSeen = function () {
+        return 0;
+    };
+    return ResultService;
+}());
+exports.ResultService = ResultService;
+//# sourceMappingURL=result.service.js.map
